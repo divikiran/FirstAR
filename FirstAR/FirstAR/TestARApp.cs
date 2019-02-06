611a48b98f085899039b63b9fbefd6b3e4837a88
Divikiran Ravela <divikiran.ravela@kmart.com.au>
2019-02-06 14:49:19 +1100

    First checkin

---
+++ FirstAR/FirstAR/TestARApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ARKit;
+using Urho;
+using Urho.iOS;
+using Urho.Shapes;
+
+namespace FirstAR
+{
+    public class TestARApp : SimpleApplication
+    {
+        public TestARApp(ApplicationOptions options) : base(options)
+        {
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            try
+            {
+                var sphereNode = Scene.CreateChild();
+                sphereNode.Position = new Vector3(0, 0, 0.5f);
+                sphereNode.SetScale(0.1f); //0.1 is 10% of its original size
+
+                var sphere = sphereNode.CreateComponent<Sphere>();
+                sphere.Color = Color.Blue;
+
+                var arKitComponent = Scene.CreateComponent<ARKitComponent>();
+                arKitComponent.Orientation = UIKit.UIInterfaceOrientation.Portrait;
+                arKitComponent.ARConfiguration = new ARWorldTrackingConfiguration()
+                {
+                    PlaneDetection = ARPlaneDetection.Horizontal,
+                };
+                arKitComponent.RunEngineFramesInARKitCallbakcs = Options.DelayedStart;
+                arKitComponent.Run();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
